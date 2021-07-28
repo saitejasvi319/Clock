8ffb592122ea8ab0eb6f96e44ae6467b64b05a34
Sai Tejasvi <saitejasvi@emind.ai>
2021-07-28 10:02:12 +0530

    Added styling and changed script code

--- clock.js
+++ clock.js
@@ -1,8 +1,28 @@
-const clock = document.getElementById("clock");
+function updateClock(){
+    
+    let currentTime = new Date();
 
-function updateTime() {
-    let currentDate = new Date();
-    let currentTime = currentDate.toLocaleTimeString();
-    clock.innerHTML = currentTime;
+  
+    let currentHour = currentTime.getHours();
+    let currentMinutes = currentTime.getMinutes();
+    let currentSeconds = currentTime.getSeconds();
+
+    currentHour = (currentHour < 10 ? "0": "") + currentHour;
+    currentMinutes = (currentMinutes < 10 ? "0": "") + currentMinutes;
+    currentSeconds = (currentSeconds < 10 ? "0": "") + currentSeconds; 
+
+    
+    currentHour = (currentHour>12) ? currentHour - 12 : currentHour;
+    currentHour = (currentHour==0) ?  12 : currentHour;
+    
+   
+    let timeOfDay = (currentHour < 12 ) ? "AM" : "PM";
+
+
+    let currentTimeStr = currentHour + ":" + currentMinutes  
+                        + ":" + currentSeconds + " " + timeOfDay;
+
+    
+    document.getElementById("clock").innerHTML = currentTimeStr;
+
 }
-
